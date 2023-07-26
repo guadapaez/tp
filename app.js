be2ddbd39e80de2cd94ac31ae0e7b98f140df790
guadapaez <guadapaez.m@gmail.com>
2023-07-25 20:32:53 -0600

    Subiendo mercado liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+
+app.use(express.static("public"))
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  })
+
+app.get('/', function(req, res){
+    res.sendFile (path.join(__dirname, '/views/home.html'))
+})
+app.get('/register', function(req, res){
+    res.sendFile (path.join(__dirname, '/views/register.html'))
+})
+app.get('/login', function(req, res){
+    res.sendFile (path.join(__dirname, '/views/login.html'))
+})
+
